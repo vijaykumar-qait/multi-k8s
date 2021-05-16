958ece71df30a65dd717e15723999a4e5afaaf8b
vijay kumar <vijay.kumar@hotstar.com>
2021-05-16 21:22:10 +0530

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t vijaykumar23/multi-client:latest -t vijaykumar23/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t vijaykumar23/multi-server:latest -t vijaykumar23/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t vijaykumar23/multi-worker:latest -t vijaykumar23/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push vijaykumar23/multi-client:latest
+docker push vijaykumar23/multi-server:latest
+docker push vijaykumar23/multi-worker:latest
+
+docker push vijaykumar23/multi-client:$SHA
+docker push vijaykumar23/multi-server:$SHA
+docker push vijaykumar23/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=vijaykumar23/multi-server:$SHA
+kubectl set image deployments/client-deployments client=vijaykumar23/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=vijaykumar23/multi-worker:$SHA
