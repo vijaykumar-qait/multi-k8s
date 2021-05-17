7362d805de11b6fc5725d788db51b1b564494d48
vijay kumar <vijay.kumar@hotstar.com>
2021-05-17 09:18:59 +0530

    new change

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib calculator version 2
+            Fib calculator version K8s
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
